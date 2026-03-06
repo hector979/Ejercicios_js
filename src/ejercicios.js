3d97a8df73308abc87693741a90df1779e802947
Hector979 <you@example.com>
2026-03-05 21:14:25 -0500

    28 de 35

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -117,7 +117,7 @@ function esPar(num) {
 	// Devuelve "true" si "num" es par
 	// De lo contrario, devuelve "false"
 	// Tu código:
-	return num /2===0;
+	return (num) %2===0;
 }
 
 // Test 18
@@ -125,7 +125,7 @@ function esImpar(num) {
 	// Devuelve "true" si "num" es impar
 	// De lo contrario, devuelve "false"
 	// Tu código:
-	return num /2===1;
+	return (num) %2===1;
 }
 
 // Test 19
@@ -148,18 +148,21 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
 	// Devuelve el valor de "num" elevado al exponente dado en "exponent"
 	// Tu código:
+	return num ** exponent;
 }
 
 // Test 22
 function redondearNumero(num) {
 	// Redondea "num" al entero más próximo y devuélvelo
 	// Tu código:
+	return Math.round(num);
 }
 
 // Test 23
 function redondearHaciaArriba(num) {
 	// Redondea "num" hacia arriba (al próximo entero) y devuélvelo
 	// Tu código:
+	return Math.ceil(num);
 }
 
 // Test 24
@@ -167,6 +170,7 @@ function numeroRandom() {
 	//Generar un número al azar entre 0 y 1 y devolverlo
 	//Pista: investigá qué hace el método Math.random()
 	// Tu código:
+	return Math.random();
 }
 
 // Test 25
@@ -176,7 +180,7 @@ function esPositivo(numero) {
 	//Si el número es negativo, devolver ---> "Es negativo"
 	//Si el número es 0, devuelve false
 	// Tu código:
-	return numero>=0
+	return numero >=0;
 }
 
 // Test 26
@@ -184,6 +188,7 @@ function agregarSimboloExclamacion(str) {
 	// Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
 	// Ejemplo: "hello world" pasaría a ser "hello world!"
 	// Tu código:
+	return (str)`hola !`;
 }
 
 // Test 27
@@ -198,24 +203,28 @@ function obtenerSaludo(nombre) {
 	// Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
 	// "Martin" -> "Hola Martin!"
 	// Tu código:
+	return `${nombre}hola!`
 }
 
 // Test 29
 function obtenerAreaRectangulo(alto, ancho) {
 	// Retornar el area de un rectángulo teniendo su altura y ancho
 	// Tu código:
+	return alto*ancho;
 }
 
 // Test 30
 function retornarPerimetro(lado) {
 	//Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
 	//Escribe tu código aquí
+	return lado*4;
 }
 
 // Test 31
 function areaDelTriangulo(base, altura) {
 	//Desarrolle una función que calcule el área de un triángulo.
 	//Escribe tu código aquí
+	return (base*altura)/2;
 }
 
 // Test 32
@@ -223,6 +232,7 @@ function deEuroAdolar(euro) {
 	//Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
 	//como parámetro un número de euros y calcule el cambio en dólares.
 	//Escribe tu código aquí
+	return euro*1.2;
 }
 
 // Test 33
@@ -232,7 +242,9 @@ function esVocal(letra) {
 	//que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
 	// Si no es vocal, tambien debe devolver "Dato incorrecto".
 	//Escribe tu código aquí
-}
+}	
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
